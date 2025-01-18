ac8c1aaccf5f5605db62c25be97531e8e214f19e
Tanishka Sharma <tanishkaratlam@gmail.com>
2025-01-18 13:50:48 +0530

    brand screen, all_product screen and sub_categories screen

---
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/product/sortable/sortable_products.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+class AllProducts extends StatelessWidget {
+  const AllProducts({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: TAppBar(title: Text('Popular Products'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(TSizes.defaultSpace),
+            child: TSortableProducts(),
+          ),
+        ),
+      ),
+    );
+  }
+}
