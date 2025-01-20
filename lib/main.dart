f95425d5b1e35c125e3ab3a9cbe75817e9a4a33e
Tanishka Sharma <tanishkaratlam@gmail.com>
2025-01-21 02:33:15 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
+import 'package:t_store/firebase_options.dart';
 
 // Entry point of Flutter app
-void main() {
+Future<void> main() async {
+  /// Initialize Firebase & Authentication Repository
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
   runApp(const App());
 }
